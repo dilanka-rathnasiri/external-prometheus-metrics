033ff95cb0aec01cdb10b4a13bca4f4ecbbf7718
Dilanka Rathnasiri <rdilanka17@gmail.com>
2024-09-01 22:48:35 +0530

    add basic prometheus docker commands

---
+++ prometheus-server/apply.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+# start docker image
+docker run --name prometheus -d -p 127.0.0.1:9090:9090 prom/prometheus
---
+++ prometheus-server/destroy.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+# stop running docker container
+docker container stop prometheus
+
+# remove running docker container
+docker container rm prometheus
